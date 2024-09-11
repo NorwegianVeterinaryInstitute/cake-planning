bc3da05676a1f42e5b730d08762daaa43d4a8fae
novica <nnovica@gmail.com>
2024-09-11 15:56:57 +0200

    update colnames

--- mod_mainPanel.R
+++ mod_mainPanel.R
@@ -5,24 +5,35 @@ library(DT)
 mainPanelUI <- function(id) {
   ns <- NS(id)
   
-  tagList(
-    DTOutput(ns('dataTable'))
-  )
+  tagList(DTOutput(ns('dataTable')))
 }
 
 # Main Panel Server Module
 mainPanelServer <- function(id, board) {
   moduleServer(id, function(input, output, session) {
-    
     output$dataTable <- renderDT({
       # Read the pinned data frame from the pin board
-      pinned_cakes <- pin_reactive_read(board, name = paste0(Sys.getenv("user_name"),'/cake_user_inputs'), interval = 1000)
-      datatable(pinned_cakes(), options = list(
-        columnDefs = list(list(
-          targets = '_all',
-          className = 'dt-center'
+      pinned_cakes <-
+        pin_reactive_read(
+          board,
+          name = paste0(Sys.getenv("user_name"), '/cake_user_inputs'),
+          interval = 1000
+        )
+      datatable(
+        pinned_cakes(),
+        colnames = c(
+          "Date",
+          "Hour",
+          "Room",
+          "Section",
+          "Person Name",
+          "Cake Description"
+        ),
+        options = list(columnDefs = list(
+          list(targets = '_all',
+               className = 'dt-center')
         ))
-      ))
+      )
     })
   })
 }
